773ecd7fafaf06e131bc78a812fcbae430c64efa
kurigomecha <rey22test@gmail.com>
2022-10-28 16:53:52 +0400

    Add validation function and RegEx

--- validation.js
+++ validation.js
@@ -1,5 +1,27 @@
 // validation script here
 
+const inputs = document.querySelectorAll(`input`);
+
 const patterns = {
-  telephone: /^\d{11}$/
+  email:     /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  password:  /^[\w@-]{8,20}$/i,
+  slug:      /^[a-z\d-]{8,20}$/,
+  telephone: /^\d{11}$/,
+  username:  /^[a-z\d]{5,12}$/i
 };
+
+// validation function
+function validate(field, regex){
+  if(regex.test(field.value)){
+    field.className = `valid`;
+  } else {
+    field.className = `invalid`;
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener(`keyup`, (e) => {
+    // console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value])
+  });
+});
